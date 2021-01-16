e6d61d0308cf53effc3aebf2d7c9bea9fbc1bb77
iMannys <peteregeberg123@gmail.com>
2021-01-16 23:07:42 +0100

    discordtoroblox

--- API/routes.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(app){
-    require("./sub-routes/get-data")(app)
-    require("./sub-routes/save-data")(app)
-}
--- index.js
+++ index.js
@@ -5,63 +5,26 @@ const mongoose = require('mongoose')
 const express = require('express')
 const settings = require("./settings.json")
 
-
-
-
-
-
-
 const app = express()
-
-const username = settings.USERNAME
-const passwordAuth = settings.PASSWORD
 
 const mongoPass = settings.MONGO_PASS
 const mongoUser = settings.MONGO_USER
 const DBname = settings.MONGO_DBName
 
+//mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPass}@discordtoroblox.in2nf.mongodb.net/${DBname}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true });
 
+var port = 8000
 
+app.use(bodyparser.text())
 
-
-
-mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPass}@discordtoroblox.in2nf.mongodb.net/${DBname}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true });
-
-var db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error:"));
-
-db.once("open", function() {
-    console.log("Connection To MongoDB Atlas Successful!");
-});
-
-app.use(express.json());
-app.use((req, res, next) => {  
-    const auth = {login: username, password: passwordAuth}
-
-    
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-
-   
-    if (login && password && login === auth.login && password === auth.password) {
-        return next()
-    }
-
-    res.set('WWW-Authenticate', 'Basic realm="401"')
-    res.status(401).send('Authentication required.')
+app.post('/post', (request, response) => {
+    response.send("Gotten POST request")
+    console.log(request.body)
 })
 
-
-require('./API/routes')(app);
-
-
-const PORT = 3000
-app.listen(PORT, () => {
-    console.log("> App Listening to", PORT)
+app.listen(port, function(){
+    console.log(`started server at http://localhost:${port}`)
 })
-
-
 
 bot.login(process.env.DJS_TOKEN);
 
