4b93058b12332f248ca677dd732cecf73480597c
iMannys <peteregeberg123@gmail.com>
2021-01-17 01:13:21 +0100

    web

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose')
 const express = require('express')
 const web = require("./web")
 
-
-var method = /\d+/g
 var str = web.res
-var res = parseInt(str.match(method))
-console.log(res)
+while (str !== "undefined"){
+    var method = /\d+/g
+    var res = parseInt(str.match(method))
+    console.log(res)
+}
+
 
 bot.login(process.env.DJS_TOKEN);
 
